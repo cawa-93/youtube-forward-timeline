b3851045f454665a33045d5c911886a0fc4c3cfc
Alex Kozack <kozackunisoft@gmail.com>
2020-06-03 16:02:28 +0300

    Config

---
+++ web-ext-config.js
@@ -0,0 +1,22 @@
+const {resolve} = require('path');
+
+module.exports = {
+	verbose: false,
+
+	sourceDir: resolve(__dirname, './src'),
+	artifactsDir: resolve(__dirname, './dist'),
+
+	build: {
+		overwriteDest: true,
+	},
+
+	run: {
+		startUrl: ['https://www.youtube.com/watch?v=RFTzqk7QG3U'],
+		browserConsole: true,
+	},
+
+	lint: {
+		warningsAsErrors: process.env.GITHUB_ACTIONS || false
+	}
+
+};
