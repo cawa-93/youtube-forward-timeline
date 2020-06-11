8727f690a9bf36c61a1aae1d06aa1707876aba4e
Alex Kozack <kozackunisoft@gmail.com>
2020-06-11 14:12:06 +0300

    Исправлена проблема с включением кнопки при переходе с видео на видео

--- src/content.js
+++ src/content.js
@@ -1,115 +1,131 @@
-const app = document.body.querySelector('ytd-app');
-
-
 /**
- *
- * @param {HTMLElement} description
- * @return {number[]}
+ * @param {number} t
+ * @return {Promise<void>}
  */
-function getChapters(description) {
-	const id = new URL(location.href).searchParams.get('v');
-	return Array
-		.from(description.querySelectorAll('a[href*="/watch"][href*="t="][href*="v=' + id + '"]'))
-		.map(a => {
-			const u = new URL(a.href);
-			const time = u.searchParams.get('t');
-			return parseInt(time, 10);
-		})
-		.filter(t => !isNaN(t) && t > 0);
+function wait(t) {
+	return new Promise(r => setTimeout(r, t));
 }
 
 
 /**
- *
- * @param {number[]} times
- * @param {number} currentTime
- * @return {number}
- */
-function getClosestChapter(times, currentTime) {
-	return Math.min(...times.filter(t => t > currentTime));
-}
-
-
-/**
- *
  * @return {Promise<HTMLElement>}
  */
-function descriptionLoaded() {
-	return new Promise(resolve => {
-		let intervalId = setInterval(() => {
+async function getApp() {
+	while (true) {
 
-			/**
-			 *
-			 * @type {HTMLElement}
-			 */
-			const description = app.querySelector('#description');
-			if (description && description.textContent.trim() !== '') {
-				clearInterval(intervalId);
-				resolve(description);
-			}
+		/** @type {HTMLElement} */
+		const app = document.body.querySelector('#content ytd-watch-flexy');
 
-		}, 500);
-	});
+		if (app !== null) {
+			return app;
+		}
+
+		await wait(100);
+	}
 }
 
 
-/**
- *
- * @return {HTMLButtonElement}
- */
-function getNextChapterButton() {
-	let next = app.querySelector('#next-timeline');
-	if (next) {
+getApp().then(app => {
+
+	/**
+	 * @param {HTMLElement} description
+	 * @return {number[]}
+	 */
+	function getChapters(description) {
+		const id = new URL(location.href).searchParams.get('v');
+		return Array
+			.from(description.querySelectorAll('a[href*="/watch"][href*="t="][href*="v=' + id + '"]'))
+			.map(a => {
+				const u = new URL(a.href);
+				const time = u.searchParams.get('t');
+				return parseInt(time, 10);
+			})
+			.filter(t => !isNaN(t) && t > 0);
+	}
+
+
+	/**
+	 * @param {number[]} times
+	 * @param {number} currentTime
+	 * @return {number}
+	 */
+	function getClosestChapter(times, currentTime) {
+		return Math.min(...times.filter(t => t > currentTime));
+	}
+
+
+	/**
+	 * @return {Promise<HTMLElement>}
+	 */
+	async function descriptionLoaded() {
+		while (true) {
+			/** @type {HTMLElement} */
+			const description = app.querySelector('#description');
+			if (description !== null && description.textContent.trim() !== '') {
+				return description;
+			}
+			await wait(100);
+		}
+	}
+
+
+	/**
+	 * @return {HTMLButtonElement}
+	 */
+	function getNextChapterButton() {
+		let next = app.querySelector('#next-timeline');
+		if (next) {
+			return next;
+		}
+
+		const play = app.querySelector('.ytp-play-button');
+		next = document.createElement('button');
+		next.id = 'next-timeline';
+		next.className = 'ytp-button';
+		next.style.cssText = 'display: inline-flex;'
+		                     + 'justify-content: center;'
+		                     + 'align-items: center;'
+		                     + 'vertical-align: top';
+
+		next.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" style="width: auto;height: 65%;" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0z" fill="none"/><path d="M4 18l8.5-6L4 6v12zm9-12v12l8.5-6L13 6z" fill="#ffffff"/></svg>`;
+
+		play.insertAdjacentElement('afterend', next);
 		return next;
 	}
 
-	const play = app.querySelector('.ytp-play-button');
-	next = document.createElement('button');
-	next.id = 'next-timeline';
-	next.className = 'ytp-button';
-	next.style.cssText = 'display: inline-flex;'
-	                     + 'justify-content: center;'
-	                     + 'align-items: center;'
-	                     + 'vertical-align: top';
 
-	next.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" style="width: auto;height: 65%;" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0z" fill="none"/><path d="M4 18l8.5-6L4 6v12zm9-12v12l8.5-6L13 6z" fill="#ffffff"/></svg>`;
+	/**
+	 * @return {Promise<void>}
+	 */
+	async function main() {
+		const isWatchPage = app.hasAttribute('video-id') && !app.hidden;
 
-	play.insertAdjacentElement('afterend', next);
-	return next;
-}
-
-
-/**
- *
- * @return {Promise<void>}
- */
-async function main() {
-	const isWatchPage = app.hasAttribute('is-watch-page');
-	if (!isWatchPage) {
-		return;
-	}
-
-	const description = await descriptionLoaded();
-
-	const times = getChapters(description);
-
-	if (!times.length) {
-		return;
-	}
-
-	const video = app.querySelector('#ytd-player video');
-
-	const nextChapterButton = getNextChapterButton();
-
-	nextChapterButton.onclick = () => {
-		const nextTime = getClosestChapter(times, video.currentTime);
-		if (nextTime > video.currentTime && nextTime < video.duration) {
-			video.currentTime = nextTime;
+		if (!isWatchPage) {
+			return;
 		}
-	};
-}
+
+		const description = await descriptionLoaded();
+
+		const times = getChapters(description);
+
+		if (!times.length) {
+			return;
+		}
+
+		const video = app.querySelector('#ytd-player video');
+
+		const nextChapterButton = getNextChapterButton();
+
+		nextChapterButton.onclick = () => {
+			const nextTime = getClosestChapter(times, video.currentTime);
+			if (nextTime > video.currentTime && nextTime < video.duration) {
+				video.currentTime = nextTime;
+			}
+		};
+	}
 
 
-main().catch(console.error);
-const mainObserver = new MutationObserver(main);
-mainObserver.observe(app, {attributeFilter: ['is-watch-page']});
+	main().catch(console.error);
+	const mainObserver = new MutationObserver(main);
+	mainObserver.observe(app, {attributeFilter: ['video-id', 'hidden']});
+});
